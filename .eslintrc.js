2d81dafda6fde598a49a383b5c9afdd5c1b88a6d
jonathantwite <13119611+jonathantwite@users.noreply.github.com>
2019-09-23 23:03:53 +0100

    eslint setup

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,7 +9,40 @@ module.exports = {
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+
+    "camelcase": ["error", { 
+        allow: ["store_getters", "store_actions", "store_mutations", "post_logout_redirect_uri", "response_type", "redirect_uri", "client_id"]
+    }],
+    'comma-dangle': 'error',
+    'comma-spacing': [
+        'error',
+        {
+            'before': false,
+            'after': true
+        }
+    ],
+    'comma-style': [ 'error', 'last' ],
+    'func-call-spacing': [ 'error', 'never' ],
+    'indent': ['error', 4],
+    'new-cap': [
+        'error',
+        {
+            'properties': false
+        }
+    ],
+    'no-trailing-spaces': 'warn',
+    'new-parens': 'error',
+    'no-unused-expressions': 'error',
+    'no-use-before-define': 'warn',
+    'object-curly-spacing': [ 'error', 'always' ],
+    'semi': ['error', 'always'],
+    "space-before-function-paren": ["error", {
+        "anonymous": "never",
+        "named": "never",
+        "asyncArrow": "always",
+    }],
+    "space-infix-ops": "error"
   },
   parserOptions: {
     parser: 'babel-eslint'
